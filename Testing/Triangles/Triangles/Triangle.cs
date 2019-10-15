916b7e9e65193a6885b8ed4bd95a2f74b4f768ac
Dmitry Sokolovsky <sadLoise@mail.ru>
2019-10-15 16:37:24 +0300

    UnitTesting

---
+++ Testing/Triangles/Triangles/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Triangles
+{
+    public class Triangle
+    {
+        public static bool IsTriangle(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+                return false;
+            if (a + b <= c || b + c <= a || a + c <= b)
+                return false;
+            return true;
+        }
+    }
+}
