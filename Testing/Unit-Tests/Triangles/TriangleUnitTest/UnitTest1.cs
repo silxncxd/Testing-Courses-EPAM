dce979de3724c4ce4457a5bfbfef1b44ea2bdab8
Dmitry Sokolovsky <sadLoise@mail.ru>
2019-10-22 00:59:27 +0300

    trying to clean my git

---
+++ Testing/Unit-Tests/Triangles/TriangleUnitTest/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangles;
+
+namespace TriangleUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsTriangleEquilateralTest()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(1, 1, 1));
+        }
+
+        [TestMethod]
+        public void IsTriangleRightTest()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(5, 8, 10));
+        }
+
+        [TestMethod]
+        public void IsTriangleIsoscelesTest()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(2, 2, 3));
+        }
+
+        [TestMethod]
+        public void IsTriangleAllSidesNullTest()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(0, 0, 0));
+        }
+
+        [TestMethod]
+        public void IsTriangleBadTest()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(1, 4, 7));
+        }
+
+        [TestMethod]
+        public void IsTriangleGoodTest()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(4, 5, 6));
+        }
+
+        [TestMethod]
+        public void IsTriangleOneBadSideTest()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(4, -5, 6));
+        }
+
+        [TestMethod]
+        public void IsTriangleTwoBadSidesGoodTest()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(-4, -5, 6));
+        }
+
+        [TestMethod]
+        public void IsTriangleThreeBadSidesGoodTest()
+        {
+            Assert.IsFalse(Triangle.IsTriangle(-4, -5, -6));
+        }
+
+        [TestMethod]
+        public void IsTriangleDoubleGoodTest()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(2.5, 3.5, 4.5));
+        }
+    }
+}
