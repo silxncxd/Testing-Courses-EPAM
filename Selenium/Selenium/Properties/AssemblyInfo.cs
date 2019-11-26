64af11d60d9314bb98d6d106629eb2ff7dc357ff
Dmitry Sokolovsky <sadLoise@mail.ru>
2019-11-26 16:03:42 +0300

    Webdriver

---
+++ Selenium/Selenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbc4b105-2534-4162-afb8-9b38f819e7aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Selenium/Selenium/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium
+{
+    [TestFixture]
+    public class WebTests
+    {
+        public IWebDriver webDriver;
+        WebDriverWait wait;
+
+        [SetUp]
+        public void StartBrowserAndGoToTheSite()
+        {
+            webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            webDriver.Navigate().GoToUrl("https://travel.ldz.lv/");
+        }
+
+        [TearDown]
+        public void QuitDriver()
+        {
+            webDriver.Quit();
+        }
+
+        [Test]
+        //test 1
+        public void SearchTrips()
+        {
+            wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            var departureStation = webDriver.FindElement(By.XPath("//input[@name = 'from']"));
+            departureStation.SendKeys("RĪGA PASAŽIERU");
+
+            var arrivalStation = webDriver.FindElement(By.XPath("//input[@name = 'to']"));
+            arrivalStation.SendKeys("MINSKA PASAŽIERU");
+
+            string todayDate = DateTime.Now.ToShortDateString();
+            var dateInput = webDriver.FindElement(By.XPath("//input[@name = 'departure']"));
+            dateInput.SendKeys(todayDate);
+
+            var cancelReturnTripButton = webDriver.FindElement(By.XPath("//span[@class='dotted-link js-book-tickets-oneway-trigger']"));
+            cancelReturnTripButton.Click();
+            wait.Until(condition: ExpectedConditions.ElementToBeClickable(By.CssSelector(".button.with-outlines")));
+            var sendRequestButton = webDriver.FindElement(By.CssSelector(".button.with-outlines"));
+            sendRequestButton.Click();
+
+            Assert.AreEqual("https://travel.ldz.lv/lv/booking/booking-1", webDriver.Url);
+            QuitDriver();
+        }
+
+        [Test]
+        //test 2
+        public void SameStationError()
+        {
+            wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            var departureStation = webDriver.FindElement(By.XPath("//input[@name = 'from']"));
+            departureStation.SendKeys("RĪGA PASAŽIERU");
+
+            var arrivalStation = webDriver.FindElement(By.XPath("//input[@name = 'to']"));
+            arrivalStation.SendKeys("RĪGA PASAŽIERU");
+
+            string todayDate = DateTime.Now.ToShortDateString();
+            var dateInput = webDriver.FindElement(By.XPath("//input[@name = 'departure']"));
+            dateInput.SendKeys(todayDate);
+
+            var cancelReturnTripButton = webDriver.FindElement(By.XPath("//span[@class='dotted-link js-book-tickets-oneway-trigger']"));
+            cancelReturnTripButton.Click();
+
+            wait.Until(condition: ExpectedConditions.ElementToBeClickable(By.CssSelector(".button.with-outlines")));
+            var sendRequestButton = webDriver.FindElement(By.CssSelector(".button.with-outlines"));
+            sendRequestButton.Click();
+
+            var errorText = webDriver.FindElement(By.CssSelector(".modal-title")).Text;
+            Assert.AreEqual("KĻŪDA!", errorText);
+            QuitDriver();
+        }
+    }
+}
