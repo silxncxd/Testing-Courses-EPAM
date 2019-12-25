3db523630b930debead6550ceab7fbe4cab67f75
Dmitry Sokolovsky <sadLoise@mail.ru>
2019-12-26 01:15:38 +0300

    For CI

---
+++ CI/GitHubAutomation/Services/QuestionInfoCreator.cs
@@ -0,0 +1,18 @@
+﻿using LdzTravelAutomation.Models;
+
+namespace LdzTravelAutomation.Services
+{
+    public class QuestionInfoCreator
+    {
+        public static QuestionInfo SetInvalidInfo()
+        {
+            return new QuestionInfo(InvalidTestDataReader.GetData("QuestionName"), InvalidTestDataReader.GetData("QuestionTheme"),
+                InvalidTestDataReader.GetData("QuestionEmail"), InvalidTestDataReader.GetData("Question"));
+        }
+        public static QuestionInfo SetNormalInfo()
+        {
+            return new QuestionInfo(NormalTestDataReader.GetData("QuestionName"), NormalTestDataReader.GetData("QuestionTheme"),
+                 NormalTestDataReader.GetData("QuestionEmail"), NormalTestDataReader.GetData("Question"));
+        }
+    }
+}
